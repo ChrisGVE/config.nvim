dbd40eb6766d135f2d559dd9662aa8338574bd87
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-09 17:46:39 +0100

    chore(nvim): slimmed down the config for things I actually use

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,6 @@ return {
   -- Disable plugins:
   { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
-  { "echasnovski/mini.animate", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
+  -- { "echasnovski/mini.animate", enabled = false },
+  -- { "nvimdev/dashboard-nvim", enabled = false },
 }
