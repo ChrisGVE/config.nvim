217fffcd8b5f3ef7c8bfa88a3659c5fc4f9fffc3
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-12 12:42:52 +0100

    chore(nvim): slimmed down the config to remove what I don't use

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -8,19 +8,15 @@ return {
       "esbonio", -- Sphinx
       "jinja-lsp", -- Jinja
       "lemminx", -- XML
-      "ltex-ls", -- Latex
       "prettierd", -- formatter
       "selene", -- lua
       "sonarlint-language-server", -- multiple, selected for XML
-      "swiftlint", -- Swift
       "textlint", -- text
       "textlsp", -- Text
       "typos", -- Text
       "typos-lsp", -- Text
       "vale", -- text
       "vale-ls", -- Text
-      "vim-language-server", -- VimScript
-      "vint", -- VimScript
       "woke", -- Text
       "write-good", -- text
       "xmlformatter", -- XML
@@ -28,8 +24,6 @@ return {
       "ruff", -- Python
       "mypy", -- Python
       "pydocstyle", -- Python
-      "nixpkgs-fmt", -- Nix
-      "rnix-lsp", -- Nix
     },
     auto_update = true,
     debounce_hours = 12, -- at least 12 hours between attempts to install/update
