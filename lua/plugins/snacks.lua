cbbe81649861926c77fa26ce0e772db6c762c2e0
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-13 17:28:56 +0100

    style(nvim): Changing the daskboard of nvim (WIP)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -19,19 +19,17 @@ return {
     words = { enabled = true },
     statuscolumn = { enabled = true },
     dashboard = {
-      preset = {
-        header = [[
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-      },
       sections = {
-        { section = "header" },
+        {
+          section = "terminal",
+          cmd = "figlet -f ~/.config/figlet/ansi_shadow 'LAZYVIM' | lolcat -F 0.3 -t -p 100 -f",
+          height = 6,
+          padding = 1,
+          align = "center",
+        },
+        -- { section = "header" },
         { pane = 2, height = 5, padding = 1 },
-        { icon = " ", title = "Keymaps", section = "keys", indent = 3, gap = 1, padding = 1 },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 3, gap = 1, padding = 1, pane = 1 },
         { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 3, padding = 1 },
         { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 3, padding = 1 },
         {
@@ -53,7 +51,7 @@ return {
           ttl = 5 * 60,
           indent = 3,
         },
-        { section = "startup" },
+        { section = "startup", pane = 2 },
       },
     },
     styles = {
