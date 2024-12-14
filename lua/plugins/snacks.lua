6aec57fe84650b88834d7654d55afa4574980cb2
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-14 10:32:36 +0100

    chore(brew): automatic update of the homebrew lists

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,5 +1,7 @@
-Snacks = require("snacks")
+local Snacks = require("snacks")
 vim.ui.input = Snacks.input
+
+local ltw = require("little-taskwarrior")
 
 return {
   "folke/snacks.nvim",
@@ -27,8 +29,16 @@ return {
           padding = 1,
           align = "center",
         },
-        -- { section = "header" },
-        { pane = 2, height = 5, padding = 1 },
+        {
+          icon = "",
+          title = "Tasks",
+          pane = 2,
+        },
+        {
+          text = ltw.get_snacks_dashboard_tasks(56, "dir", "special"),
+          pane = 2,
+          indent = 3,
+        },
         { icon = " ", title = "Keymaps", section = "keys", indent = 3, gap = 1, padding = 1, pane = 1 },
         { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 3, padding = 1 },
         { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 3, padding = 1 },
