26e6fff253b4f175ee8914ab771dc95792d0e317
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-17 12:44:13 +0100

    feat(nvim): Added lua specific keymap in localleader

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/which-key.nvim",
+  keys = {
+    -- lua section
+    { "<localleader><space>x", "<cmd>source %<cr>", mode = "n", ft = "lua", desc = "Execute file" },
+    { "<localleader>x", ":.lua<cr>", mode = "n", ft = "lua", desc = "Execute line" },
+    { "<localleader>x", ":lua<cr>", mode = "v", ft = "lua", desc = "Execute selection" },
+  },
+}
