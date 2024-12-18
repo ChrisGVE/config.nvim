0c875844f6b45e70fcca07748d20cb499ecdd189
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-18 19:44:35 +0100

    fix(nvim): excluded data/state/cache dirs from project.nvim

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,6 +1,11 @@
+local DATA = os.getenv("XDG_DATA_HOME")
+local STATE = os.getenv("XDG_STATE_HOME")
+local CACHE = os.getenv("XDG_CACHE_HOME")
+
 return {
   "ahmedkhalf/project.nvim",
   opts = {
     manual_mode = false,
+    exclude_dirs = { DATA, STATE, CACHE },
   },
 }
