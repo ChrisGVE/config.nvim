922e6eeae580ed438a46a63440a1fbe9283f61bd
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-22 19:38:13 +0100

    fix(nvim): return {} when tasktamer is not local

--- lua/plugins/tasktamer.lua
+++ lua/plugins/tasktamer.lua
@@ -1,5 +1,9 @@
-return {
-  "tasktamer.nvim",
-  dev = true,
-  opts = {},
-}
+if vim.uv.fs_stat("~/dev/projects/plugins/tasktamer.nvim/lazy.lua") then
+  return {
+    "tasktamer.nvim",
+    dev = true,
+    opts = {},
+  }
+else
+  return {}
+end
