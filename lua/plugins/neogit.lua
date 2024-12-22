9d97c9d6e518a5e37b03af99dd98ef7ae78ee6c9
Christian C. Berclaz <christian.berclaz@mac.com>
2024-12-22 20:34:50 +0100

    feat(nvim): Added neogit if lazygit is not available

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,17 @@
+if vim.fn.executable("lazygit") == 1 then
+  return {}
+else
+  return {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+      "echasnovski/mini.pick", -- optional
+    },
+    config = true,
+  }
+end
