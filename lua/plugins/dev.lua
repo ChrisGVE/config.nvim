79a2748974f2d1b90e89c016edb690657abe6bdb
Christian C. Berclaz <christian.berclaz@mac.com>
2025-01-02 19:28:16 +0100

    chore(nvim): created a single dev.lua for all plugins dev.

---
+++ lua/plugins/dev.lua
@@ -0,0 +1,32 @@
+return {
+  -- { dir = "~/dev/projects/plugins/present.nvim" },
+  { "present.nvim", dev = true },
+  {
+    "chrisgve/tasktamer.nvim",
+    dev = true,
+    config = function()
+      require("tasktamer").setup({ --- configuration for the Dashboard
+        dashboard = {
+          --- List of columns to be displayed
+          columns = {
+            "project",
+            "description",
+            "due",
+            "urgency",
+          },
+          --- List of replacements when getting lines for dashboard
+          project_replacements = {
+            ["neovim."] = "nvim.",
+            ["config."] = "cfg.",
+            ["python."] = "py.",
+            ["fountains."] = "f.",
+            ["devtools."] = "dev.",
+            ["wezterm."] = "wzt.",
+            ["work."] = "wk.",
+            ["personal."] = "pers.",
+          },
+        },
+      })
+    end,
+  },
+}
