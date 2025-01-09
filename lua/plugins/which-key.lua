cbf68dcdab70cb4918ec86a04174f8c44e411cc4
Christian C. Berclaz <christian.berclaz@mac.com>
2025-01-09 04:00:14 +0100

    chore(brew): automatic update of the homebrew lists

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,5 +5,7 @@ return {
     { "<localleader>X", "<cmd>source %<cr>", mode = "n", ft = "lua", desc = "Execute file" },
     { "<localleader>x", ":.lua<cr>", mode = "n", ft = "lua", desc = "Execute line" },
     { "<localleader>x", ":lua<cr>", mode = "v", ft = "lua", desc = "Execute selection" },
+    { "<leader>tp", "<cmd>PlenaryBustedFile<cr>", mode = "n", ft = "lua", desc = "Plenary busted file" },
+    { "<leader>tP", "<cmd>PlenaryBustedDirectory<cr>", mode = "n", ft = "lua", desc = "Plenary busted directory" },
   },
 }
