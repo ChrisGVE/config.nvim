e04fa61b82edaf4b3a1186405bcb313a6869607e
Christian C. Berclaz <christian.berclaz@mac.com>
2025-02-07 15:53:32 +0100

    feat(nvim): Added Neogit to the configuration

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,17 +1,31 @@
-if vim.fn.executable("lazygit") == 1 then
-  return {}
-else
-  return {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
+-- if vim.fn.executable("lazygit") == 1 then
+--   return {}
+-- else
+--   return {
+--     "NeogitOrg/neogit",
+--     dependencies = {
+--       "nvim-lua/plenary.nvim", -- required
+--       "sindrets/diffview.nvim", -- optional - Diff integration
+--
+--       -- Only one of these is needed.
+--       "nvim-telescope/telescope.nvim", -- optional
+--       "ibhagwan/fzf-lua", -- optional
+--       "echasnovski/mini.pick", -- optional
+--     },
+--     config = true,
+--   }
+-- end
 
-      -- Only one of these is needed.
-      "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua", -- optional
-      "echasnovski/mini.pick", -- optional
-    },
-    config = true,
-  }
-end
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
+
+    -- Only one of these is needed.
+    -- "nvim-telescope/telescope.nvim", -- optional
+    -- "ibhagwan/fzf-lua", -- optional
+    -- "echasnovski/mini.pick", -- optional
+  },
+  config = true,
+}
