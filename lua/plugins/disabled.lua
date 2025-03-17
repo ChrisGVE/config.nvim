6b37beeaef3a3cb01d16ab49b1502f19882470c2
Christian C. Berclaz <christian.berclaz@mac.com>
2025-03-17 16:03:51 +0100

    Added go language support and disabled flash.nvim

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 return {
   -- Disable plugins:
   { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
   -- { "Praczet/little-taskwarrior.nvim", enabled = false },
   -- { "echasnovski/mini.comment", enabled = false },
   -- { "echasnovski/mini.animate", enabled = false },
