8e6b0b30dcbdb15c3399919178cd37f4586192a9
Christian C. Berclaz <christian.berclaz@mac.com>
2025-04-30 22:49:33 +0200

    Modified the go support

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -5,7 +5,9 @@ return {
     "neovim/nvim-lspconfig",
     "nvim-treesitter/nvim-treesitter",
   },
-  opts = {},
+  opts = {
+    lsp_cfg = false,
+  },
   event = { "CmdLineEnter" },
   ft = { "go", "gomod" },
   build = ':lua require("go.install").update_all_sync()', -- If you need to install/update
--- lua/plugins/mason.lua
+++
@@ -1,7 +0,0 @@
--- Integration of go.nvim with Mason
---
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("go").setup({ lsp_cfg = false })
-local cfg = require("go.lsp").config()
-require("lspconfig").gopls.setup(cfg)
